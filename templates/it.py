7bcad905815b2f0fa3fa47f3f72a98f1012cb358
Luca Postregna <luca.postregna@gmail.com>
2011-07-19 20:37:45 +0200

    add sitemap

--- templates/it.py
+++ templates/it.py
@@ -3,7 +3,7 @@ import datetime
 language = "it"
 keywords = "b&amp;b, bed and breakfast, casa vacanze, draha, oasi di pace, postregna, teresa, cividale, friuli venezia giulia, fvg, valli del natisone, dormire, soggiornare, stregna, udine"
 description = "Draha Casa Vacanze e B&amp;B Oasi di Pace di Teresa Postregna a Stregna nelle Valli del Natisone"
-site_name = "Draha Casa vacanze - Oasi di Pace B&amp;B"
+site_name = "Draha Casa Vacanze - Oasi di Pace B&amp;B"
 src_dir = "src/it"
 dst_dir = "it"
 execfile('templates/default.py')
