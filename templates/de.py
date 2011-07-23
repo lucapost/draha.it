e2ef27c56e5ab0f911964b5d69b7d8ac694de2e7
Luca Postregna <luca.postregna@gmail.com>
2011-07-23 14:28:41 +0200

    fix title

--- templates/de.py
+++ templates/de.py
@@ -3,7 +3,7 @@ import datetime
 language = "de"
 keywords = "b&amp;b, bed and breakfast, ferienhaus, draha, die friedens oase, postregna, teresa, cividale, friuli venezia giulia, fvg, natisone, taelern des natisone, zimmer, stregna, udine"
 description = "Ferienhaus und Bed and Breakfast in Stregna in den Talern des Natisone"
-site_name = "Draha Ferienhaus, Die Friedens Oase B&amp;B"
+site_name = "Draha Ferienhaus, Die Friedens Oase Bed and Breakfast"
 src_dir = "src/de"
 dst_dir = "de"
 execfile('templates/default.py')
--- templates/it.py
+++ templates/it.py
@@ -3,7 +3,7 @@ import datetime
 language = "it"
 keywords = "b&amp;b, bed and breakfast, casa vacanze, draha, oasi di pace, postregna, teresa, cividale, friuli venezia giulia, fvg, valli del natisone, dormire, soggiornare, stregna, udine"
 description = "B&amp;B e casa vacanze a Stregna nelle Valli del Natisone"
-site_name = "Draha Casa Vacanze - Oasi di Pace B&amp;B"
+site_name = "Draha Casa Vacanze - Oasi di Pace Bed and Breakfast"
 src_dir = "src/it"
 dst_dir = "."
 execfile('templates/default.py')
