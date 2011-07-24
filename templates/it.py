6175dfe93542f8eba8e5cbf6d3f2a1ea8b802c8f
Luca Postregna <luca.postregna@gmail.com>
2011-07-24 12:05:22 +0200

    fix

--- templates/it.py
+++ templates/it.py
@@ -2,7 +2,7 @@ import datetime
 
 language = "it"
 keywords = "b&amp;b, bed and breakfast, casa vacanze, draha, oasi di pace, postregna, teresa, cividale, friuli venezia giulia, fvg, valli del natisone, dormire, soggiornare, stregna, udine"
-description = "B&amp;B e casa vacanze a Stregna nelle Valli del Natisone"
+description = "B&amp;B e casa vacanze, dormire e soggiornare a Stregna e nelle Valli del Natisone"
 site_name = "Draha Casa Vacanze - Oasi di Pace Bed and Breakfast"
 src_dir = "src/it"
 dst_dir = "."
