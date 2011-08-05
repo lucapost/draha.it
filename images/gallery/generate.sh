1ae1f06a7c88fd337664cc409710a9b57d9a1554
Luca Postregna <luca.postregna@gmail.com>
2011-08-05 19:26:10 +0200

    fix

--- images/gallery/generate.sh
+++ images/gallery/generate.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-#sed '/^$/d' $1/data.txt > tmp.txt
+mkdir thumbs
 
-#exec 3<&0
-#exec 0<tmp.txt
+for a in $1/*.jpg  ; do
+	name=`echo $a |sed s'/.jpg//'`
+	echo $name
+        convert -thumbnail !60 -unsharp 0x.5 $a -path thumbs $name.jpg 
+done
+exec 3<&0
+exec 0<data.txt
 
-#while read line; do
-
-for i in *.jpg ; do
-
-	name=$i
+while read line; do
 
 	name=`echo $line | cut -d: -f1`
 	altit=`echo $line | cut -d: -f2`
 	titleit=`echo $line | cut -d: -f3`
 
-	echo "<li><a href=\"/images/gallery/full/$name\" title=\"$titleit\"><img src=\"/images/gallery/thumbs/$name\" alt=\"$altit\" title=\"$titleit\" /></a></li>"  >> tmp/it.txt
+	echo "<li><a href=\"/images/gallery/full/$name\" title=\"$titleit\"><img src=\"/images/gallery/thumbs/$name\" alt=\"$altit\" title=\"$titleit\" /></a></li>"  >> tmp.txt
 
 done
 
-#exec 0<&3
+exec 0<&3
 
-cat tmp/it.txt
+cat tmp.txt
+rm tmp.txt
